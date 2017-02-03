6107917daa5602ddc9f03e38b10b505745b65601
Daniyal Gabitov <danuy@list.ru>
2017-02-03 09:38:30 +0300

    Add new rules, and code examples for eslint

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -4,8 +4,28 @@ let a = 10;
 let b = 10;
 a == b;
 
+a | b;
+
+if (true) alert('hi');
+
 document.addEventListener('DOMContentLoaded', () => {
  document.querySelector('button').addEventListener('click', () => {
     let myObject = new MyClass();
   });
 });
+
+function foo() {
+  var callee = arguments.callee;
+}
+
+let c = {
+  property: '123'
+};
+
+c['property'];
+
+(b = 3) && foo();
+
+var foo = { 'bar': 'This is a bar.', 'baz': { 'qux': 'This is a qux' }, 'difficult': 'to read' };
+
+var a = function() {};
